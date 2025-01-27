60f28ab57e5213ef3917756bd4e7c531eecd800e
Doruk Duman <idduman@hotmail.com>
2025-01-28 00:45:49 +0300

    [Added] startle animations

--- Assets/Scripts/LoadingSceneManager.cs
+++ Assets/Scripts/LoadingSceneManager.cs
@@ -1,7 +1,13 @@
+using System;
 using UnityEngine;
 
 public class LoadingSceneManager : MonoBehaviour
 {
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     public void LoadGame()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("CatScene");
